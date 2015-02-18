fb60b19c568999c1c8e5fd141e64527a330f2460
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-18 11:21:06 -0800

    Add jquery and ternary operator

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,38 @@ var BankAccount = {
     this.balance = this.balance + amount;
   }
 };
+
+$(document).ready(function() {
+  $("form#new-name").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedName = $("input#new-name").val();
+    var initialDeposit = parseFloat($("input#initial-deposit").val());
+    var newAccount = Object.create(BankAccount);
+    newAccount.name = inputtedName;
+    newAccount.deposit(initialDeposit);
+
+    $("input#new-name").val("");
+    $("input#initial-depsoit").val("");
+
+    $("#box2").show();
+    $("#account").show();
+    $(".name").text(newAccount.name);
+    $(".balance").text(newAccount.balance);
+
+    $("form#deposit-withdraw").submit(function(event) {
+      event.preventDefault();
+
+      var inputtedDeposit = $("input#deposit-amount").val().length ? parseFloat($("input#deposit-amount").val()) : 0;
+      var inputtedWithdraw = $("input#withdraw-amount").val().length ? parseFloat($("input#withdraw-amount").val()) : 0;
+
+      newAccount.deposit(inputtedDeposit);
+      newAccount.withdraw(inputtedWithdraw);
+
+      $("input#deposit-amount").val("");
+      $("input#withdraw-amount").val("");
+
+      $(".balance").text(newAccount.balance);
+    });
+  });
+});
